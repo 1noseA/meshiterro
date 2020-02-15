d3eeb3ff08855a329d36adc9473ae4ff23053ebb
1noseA <1noseA2L@gmail.com>
2020-02-15 18:06:44 +0900

    [Finish]5-3

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,16 +2,20 @@ class PostImagesController < ApplicationController
   def new
   	@post_image = PostImage.new
   end
+
   def create
   	@post_image = PostImage.new(post_image_params)
-  	@post_image.user_id = current_user.user_id
+  	@post_image.user_id = current_user.id
   	@post_image.save
   	redirect_to post_images_path
   end
+
   def index
   	@post_images = PostImage.all
   end
+
   def show
+  	@post_image = PostImage.find(params[:id])
   end
 
   private
